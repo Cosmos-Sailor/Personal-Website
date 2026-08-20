d9f061057f5ea5d8451ada5b347c7443e48c5307
Cosmos-Sailor <127806407+Cosmos-Sailor@users.noreply.github.com>
2026-08-20 15:45:58 -0400

    Update ProjectDetails to make description a carousel.js

--- src/pages/Projects/ProjectDetails.js
+++ src/pages/Projects/ProjectDetails.js
@@ -92,18 +92,37 @@ const ProjectDetails = () => {
                     })}
                 </span>
             </Box>
-
-            <Box sx={{textAlign: 'center', backgroundColor: '#f0f0f0', borderRadius: '15px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)' }}>
-            <h3 style={{fontSize: 46, padding: '10px', marginBottom: '10px'}}>Project Overview</h3>
-                <div style={{marginTop:"-50px", display: 'flex', flexDirection: 'column', gap: '0px', textAlign: 'left', paddingLeft: 0, padding: '1.25rem'}}>
-                    <h3 style={{fontSize: 22, marginBottom: '0px'}}>Project Description</h3>
-                    <h3 style={{fontSize: 16, marginBottom: '0px', fontWeight: 'normal', whiteSpace: 'pre-wrap'}}>{projectDescription}</h3>
-                    <h3 style={{fontSize: 22, marginBottom: '0px'}}>Methodology</h3>
-                    <h3 style={{fontSize: 16, marginBottom: '0px', fontWeight: 'normal', whiteSpace: 'pre-wrap'}}>{methodology}</h3>
-                    <h3 style={{fontSize: 22, marginBottom: '0px'}}>Reflection</h3>
-                    <h3 style={{fontSize: 16, marginBottom: '0px', fontWeight: 'normal', whiteSpace: 'pre-wrap'}}>{reflection}</h3>
+                        
+            <Box sx={{textAlign: 'center', backgroundColor: '#f0f0f0', borderRadius: '15px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)', mb: 6 }}>
+                <h3 style={{fontSize: 46, padding: '15px', marginBottom: '10px'}}>Project Visuals</h3>
+                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <BsArrowLeftCircleFill onClick={handlePrev} style={{display: 'flex', padding: '15px', width: '2rem', height: '2rem', color: 'rgba(76, 80, 79, 0.42)', cursor: 'pointer'}}/>
+                    {project.blurbs.map((blurb, index) => (
+                        <div style={{borderRadius: '0.5rem', boxShadow: '0px 0px 7px #666', maxWidth: '60%', maxHeight: '400px'}}>
+                            <h3 style={{fontSize: 16, marginBottom: '0px', fontWeight: 'normal', whiteSpace: 'pre-wrap'}}>{blurb.link}</h3>
+                        </div>
+                    ))}
+                    <BsArrowRightCircleFill onClick={handleNext} style={{display: 'flex', padding: '15px', width: '2rem', height: '2rem', color: 'rgba(76, 80, 79, 0.42)', cursor: 'pointer'}}/>
                 </div>
+                <h3 style={{display: 'flex',  justifyContent: 'center', fontSize: 20}}>{project.blurbs[currentIndex].name}</h3>
+                <span style={{display: 'flex', position: 'relative', justifyContent: 'center', padding: '0px 0px 20px 0px'}}>
+                    {project.blurb.map((__, idx) => {
+                        return <button style={{backgroundColor: 'rgba(76, 80, 79, 0.42)', height: 15, width: 15, borderRadius: '100%', margin: ' 0 0.2rem', padding: '0px'}} key={idx} onClick={() => setCurrentIndex(idx)}></button>
+                    })}
+                </span>
             </Box>
+                        
+            // <Box sx={{textAlign: 'center', backgroundColor: '#f0f0f0', borderRadius: '15px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)' }}>
+            // <h3 style={{fontSize: 46, padding: '10px', marginBottom: '10px'}}>Project Overview</h3>
+            //     <div style={{marginTop:"-50px", display: 'flex', flexDirection: 'column', gap: '0px', textAlign: 'left', paddingLeft: 0, padding: '1.25rem'}}>
+            //         <h3 style={{fontSize: 22, marginBottom: '0px'}}>Project Description</h3>
+            //         <h3 style={{fontSize: 16, marginBottom: '0px', fontWeight: 'normal', whiteSpace: 'pre-wrap'}}>{projectDescription}</h3>
+            //         <h3 style={{fontSize: 22, marginBottom: '0px'}}>Methodology</h3>
+            //         <h3 style={{fontSize: 16, marginBottom: '0px', fontWeight: 'normal', whiteSpace: 'pre-wrap'}}>{methodology}</h3>
+            //         <h3 style={{fontSize: 22, marginBottom: '0px'}}>Reflection</h3>
+            //         <h3 style={{fontSize: 16, marginBottom: '0px', fontWeight: 'normal', whiteSpace: 'pre-wrap'}}>{reflection}</h3>
+            //     </div>
+            // </Box>
         </section>
     );
 };
