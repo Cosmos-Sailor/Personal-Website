26cae542c885823d8d497e3b00a8d070e229ea3d
Cosmos-Sailor <127806407+Cosmos-Sailor@users.noreply.github.com>
2026-08-24 17:24:39 +0000

    rerouting variable

--- src/pages/Projects/ProjectDetails.js
+++ src/pages/Projects/ProjectDetails.js
@@ -83,7 +83,7 @@ const ProjectDetails = () => {
                 <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                     <BsArrowLeftCircleFill onClick={handlePrevBlurb} style={{display: 'flex', padding: '15px', width: '2rem', height: '2rem', color: 'rgba(76, 80, 79, 0.42)', cursor: 'pointer'}}/>
                     {project.blurbs.map((blurb, index) => (
-                        <zero-md className={currentBlurbIndex === index ? "slide" : "slide-hidden"} src={currentCarouselText} style={{ textAlign: 'left', fontSize:18, lineHeight: 1.6, maxWidth: '80%'}}></zero-md>
+                        <zero-md className={currentBlurbIndex === index ? "slide" : "slide-hidden"} src={project.blurb} style={{ textAlign: 'left', fontSize:18, lineHeight: 1.6, maxWidth: '80%'}}></zero-md>
                     ))}
                     <BsArrowRightCircleFill onClick={handleNextBlurb} style={{display: 'flex', padding: '15px', width: '2rem', height: '2rem', color: 'rgba(76, 80, 79, 0.42)', cursor: 'pointer'}}/>
                 </div>
