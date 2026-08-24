9922c7839ecbacf89a42e1d2214578142700051f
Cosmos-Sailor <127806407+Cosmos-Sailor@users.noreply.github.com>
2026-08-24 18:29:00 +0000

    trying to make md file load

--- src/pages/Projects/ProjectDetails.js
+++ src/pages/Projects/ProjectDetails.js
@@ -4,9 +4,9 @@ import { Box } from '@mui/material';
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill} from 'react-icons/bs';
 import projectConfig from '../../assets/configs/projectDetailsConfigs'
 import "./ProjectDetails.css"
-import ZeroMd from 'https://cdn.jsdelivr.net/npm/zero-md@3'
+//import ZeroMd from 'https://cdn.jsdelivr.net/npm/zero-md@3'
 
-customElements.define('zero-md', ZeroMd)
+//customElements.define('zero-md', ZeroMd)
 
 const ProjectDetails = () => {
     const { projectId } = useParams();
@@ -61,6 +61,9 @@ const ProjectDetails = () => {
 
     return (
         <section id='project-details'>
+            <head>
+                <script type="module" src="https://cdn.jsdelivr.net/npm/zero-md@3?register"></script>
+            </head>
             <Box sx={{ textAlign: 'center', py: 4, backgroundColor: '#f0f0f0', marginTop: '10px', borderRadius: '15px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)', mb: 6 }}>
                 <div style={{marginTop:"-75px", display: 'flex', flexDirection: 'column', gap: '0px', textAlign: 'center', paddingLeft: 0, padding: '1.25rem'}}>
                     <h3 style={{fontSize: 46, marginBottom: '0px'}}>{project.title}</h3>
