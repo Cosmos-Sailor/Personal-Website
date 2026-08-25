19b2e527558a298915fb91e4562ddb0e166f9f41
Cosmos-Sailor <127806407+Cosmos-Sailor@users.noreply.github.com>
2026-08-25 19:12:42 +0000

    trying to figure out what is causing the issue

--- src/components/Cards/MiniCard.js
+++ src/components/Cards/MiniCard.js
@@ -12,8 +12,6 @@ const MiniCard = (resource) => {
                 <Box sx={{ py: 0.3 }}>
                     <Box sx={{ display: 'flex', justifyContent: 'center', mt: 0.5 }}>
                         <IconButton
-                            title={resource.descripton}
-                            href={resource.link}
                             color="primary"
                             sx={{ color: '#263238', padding: 0.5 }}
                         >
