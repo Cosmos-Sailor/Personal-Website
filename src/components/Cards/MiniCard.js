30a11b812e071082a3f48a936337aef596dbc673
Cosmos-Sailor <127806407+Cosmos-Sailor@users.noreply.github.com>
2026-08-25 20:20:37 +0000

    making text wrap

--- src/components/Cards/MiniCard.js
+++ src/components/Cards/MiniCard.js
@@ -14,6 +14,7 @@ const MiniCard = ({item}) => {
                             href={item.link}
                             color="primary"
                             sx={{ color: '#263238', padding: 0.5 }}
+                            maxWidth='80%'
                         >
                             {item.description}
                         </IconButton>
