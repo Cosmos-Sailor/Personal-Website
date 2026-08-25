10efb4d99c6723685441c7789d89f3f93cc1e3e5
Cosmos-Sailor <127806407+Cosmos-Sailor@users.noreply.github.com>
2026-08-25 19:22:06 +0000

    I am very confused

--- src/components/Cards/MiniCard.js
+++ src/components/Cards/MiniCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, Box, IconButton} from '@mui/material';
 
-const MiniCard = (resource) => {
+const MiniCard = (prop) => {
     return (
         <Card sx={{ width: 380, height: 450, m: 2, backgroundColor: '#F6F6F6', boxShadow: 3, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-            <CardMedia component={resource.media} sx={{src: `url(${resource.preview})`, height: 320, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}/>
+            <CardMedia component={prop.media} sx={{src: `url(${prop.preview})`, height: 320, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}/>
             <CardContent sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', p: 2 }}>
                 <Box sx={{ borderTop: 1, borderColor: 'divider', my: 1 }} />
                 <Box sx={{ py: 0.3 }}>
                     <Box sx={{ display: 'flex', justifyContent: 'center', mt: 0.5 }}>
                         <IconButton>
-                            {resource.description}
+                            {prop.description}
                         </IconButton>
                     </Box>
                 </Box>
