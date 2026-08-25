80ff1b5f588d99e7747e56b9af93ab2b5f7985a0
Cosmos-Sailor <127806407+Cosmos-Sailor@users.noreply.github.com>
2026-08-25 19:54:58 +0000

    more tests

--- src/components/Cards/MiniCard.js
+++ src/components/Cards/MiniCard.js
@@ -6,16 +6,19 @@ const MiniCard = (prop) => {
         <Card sx={{ width: 380, height: 450, m: 2, backgroundColor: '#F6F6F6', boxShadow: 3, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
             <CardMedia component={prop.media} sx={{src: `url(${prop.preview})`, height: 320, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}/>
             <CardContent>
+                <p style={{ color: 'red', fontSize: '20px' }}>
+                    {prop.description}
+                </p>
+
                 <Button
                     variant="contained"
                     sx={{
                         color: 'red',
                         backgroundColor: 'yellow',
                         fontSize: '20px',
-                        display: 'block'
                     }}
                 >
-                    TEST BUTTON
+                    {prop.description}
                 </Button>
             </CardContent>
         </Card>
