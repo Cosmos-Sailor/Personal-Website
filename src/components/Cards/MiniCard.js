7a6f08012dd7da228b2b771ffe8f96fbae551c2f
Cosmos-Sailor <127806407+Cosmos-Sailor@users.noreply.github.com>
2026-08-25 13:28:36 +0000

    still not working

--- src/components/Cards/MiniCard.js
+++ src/components/Cards/MiniCard.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Card, CardContent, Box } from '@mui/material';
 import CardIcons from './CardIcons';
 //CardMedia, 
-//<CardMedia component={props.media} sx={{src: `url(${props.preview})`, height: 320, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}/>
+//<CardMedia component={resource.media} sx={{src: `url(${resource.preview})`, height: 320, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}/>
 
-const MiniCard = (props) => {
+const MiniCard = (resource) => {
     return (
         <Card sx={{ width: 380, height: 450, m: 2, backgroundColor: '#F6F6F6', boxShadow: 3, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
             <CardContent sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', p: 2 }}>
                 <Box sx={{ borderTop: 1, borderColor: 'divider', my: 1 }} />
-                <CardIcons item={props.description} sx={{ py: 0.3 }} />
+                <CardIcons item={resource.description} sx={{ py: 0.3 }} />
             </CardContent>
         </Card>
     );
