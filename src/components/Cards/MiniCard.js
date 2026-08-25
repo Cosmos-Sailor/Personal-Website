f35a34f5bdceddc9ae5c79d8d51df081dda1dd1d
Cosmos-Sailor <127806407+Cosmos-Sailor@users.noreply.github.com>
2026-08-25 19:19:43 +0000

    it was not : (

--- src/components/Cards/MiniCard.js
+++ src/components/Cards/MiniCard.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import { Card, CardContent, Box, IconButton} from '@mui/material';
-
-//CardMedia, 
-//<CardMedia component={resource.media} sx={{src: `url(${resource.preview})`, height: 320, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}/>
+import { Card, CardMedia, CardContent, Box, IconButton} from '@mui/material';
 
 const MiniCard = (resource) => {
     return (
         <Card sx={{ width: 380, height: 450, m: 2, backgroundColor: '#F6F6F6', boxShadow: 3, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+            <CardMedia component={resource.media} sx={{src: `url(${resource.preview})`, height: 320, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}/>
             <CardContent sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', p: 2 }}>
                 <Box sx={{ borderTop: 1, borderColor: 'divider', my: 1 }} />
                 <Box sx={{ py: 0.3 }}>
                     <Box sx={{ display: 'flex', justifyContent: 'center', mt: 0.5 }}>
-                        <IconButton
-                            title={resource.description}
-                            href={resource.link}
-                            color="primary"
-                            sx={{ color: '#263238', padding: 0.5 }}
-                        >
+                        <IconButton>
                             {resource.description}
                         </IconButton>
                     </Box>
