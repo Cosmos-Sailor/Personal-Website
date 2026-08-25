5044fa17f93006b2f0373a9a5fa507d2ffab98d0
Cosmos-Sailor <127806407+Cosmos-Sailor@users.noreply.github.com>
2026-08-25 20:08:59 +0000

    cursed by a pair of parenthesis

--- src/components/Cards/MiniCard.js
+++ src/components/Cards/MiniCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, Button} from '@mui/material';
 
-const MiniCard = (item) => {
+const MiniCard = ({item}) => {
     return (
         <Card sx={{ width: 380, height: 450, m: 2, backgroundColor: '#F6F6F6', boxShadow: 3, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
             <CardMedia component={item.media} sx={{src: `url(${item.preview})`, height: 320, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}/>
