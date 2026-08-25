515dc8e198650c9783ea0bc02f9c97aae74f81af
Cosmos-Sailor <127806407+Cosmos-Sailor@users.noreply.github.com>
2026-08-25 19:02:15 +0000

    I was not missing a dependency oops

--- src/components/Cards/MiniCard.js
+++ src/components/Cards/MiniCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Card, CardContent, Box, IconButton} from '@mui/material';
-import CardIcons from './CardIcons';
 
 //CardMedia, 
 //<CardMedia component={resource.media} sx={{src: `url(${resource.preview})`, height: 320, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}/>
@@ -10,16 +9,17 @@ const MiniCard = (resource) => {
         <Card sx={{ width: 380, height: 450, m: 2, backgroundColor: '#F6F6F6', boxShadow: 3, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
             <CardContent sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', p: 2 }}>
                 <Box sx={{ borderTop: 1, borderColor: 'divider', my: 1 }} />
-                <Box sx={{ display: 'flex', justifyContent: 'center', mt: 0.5 }}>
-                    <IconButton
-                        key={resource.descripton}
-                        title={resource.descripton}
-                        href={resource.url}
-                        color="primary"
-                        sx={{ color: '#263238', padding: 0.5 }}
-                    >
-                        {resource.descripton}
-                    </IconButton>
+                <Box sx={{ py: 0.3 }}>
+                    <Box sx={{ display: 'flex', justifyContent: 'center', mt: 0.5 }}>
+                        <IconButton
+                            title={resource.descripton}
+                            href={resource.url}
+                            color="primary"
+                            sx={{ color: '#263238', padding: 0.5 }}
+                        >
+                            {resource.descripton}
+                        </IconButton>
+                    </Box>
                 </Box>
             </CardContent>
         </Card>
