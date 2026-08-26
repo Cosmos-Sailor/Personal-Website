0e8fc3f4db3ef2cf8396e61b2d13da6ec7f1a09a
Cosmos-Sailor <127806407+Cosmos-Sailor@users.noreply.github.com>
2026-08-26 16:56:57 +0000

    formatting continued

--- src/components/Cards/MiniCard.js
+++ src/components/Cards/MiniCard.js
@@ -3,8 +3,8 @@ import { Card, CardMedia, CardContent, Box, Button} from '@mui/material';
 
 const MiniCard = ({item}) => {
     return (
-        <Card sx={{ width: 400, height: 380, m: 2, backgroundColor: '#F6F6F6', boxShadow: 3, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-            <CardMedia component={item.media} src={item.preview} sx={{height: 250, width:'stretch', padding: 1, backgroundSize: '90%', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}} controls/>
+        <Card sx={{ width: 420, height: 400, m: 2, backgroundColor: '#F6F6F6', boxShadow: 3, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+            <CardMedia component={item.media} src={item.preview} sx={{height: 270, width:'stretch', padding: 1, backgroundSize: '90%', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}} controls/>
             <CardContent sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', p: 2, maxWidth: '100%' }}>
                 <Box sx={{ borderTop: 1, borderColor: 'divider', my: 1 }} />
                 <Box sx={{ py: 0.3 }}>
