94ede2e40de5440daa9d187e64241e706b93344c
Cosmos-Sailor <127806407+Cosmos-Sailor@users.noreply.github.com>
2026-08-27 03:04:43 +0000

    padding text in timeline

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -42,7 +42,7 @@ const CustomTimeline = (props) => {
                                 <Typography variant="subtitle2" sx={{ textAlign: 'center', fontFamily: 'Open Sans, sans-serif', color: '#757575' }}>
                                     {company}
                                 </Typography>
-                                <Typography variant="body1" sx={{ textAlign: 'left', mb: 2, fontFamily: 'Open Sans, sans-serif', color: '#424242' }}>
+                                <Typography variant="body1" sx={{ padding: 4, textAlign: 'left', mb: 2, fontFamily: 'Open Sans, sans-serif', color: '#424242' }}>
                                     {description}
                                 </Typography>
                                 <Tags id={id} tags={tags} />
